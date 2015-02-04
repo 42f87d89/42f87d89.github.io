b4d4e8e83656f649d878b7b4987c5f599d7e987b
42f87d89 <gbuschle@hotmail.com>
2015-02-04 15:54:44 +0000

    blog first post

---
+++ Blog/post1/post1.php
@@ -0,0 +1,18 @@
+I don't quite feel this is done, but I'm starting to work on other things, so if I don't write this now, I will never do it.
+
+A long long time ago, I was interested in alternative number bases. I started with binary, obviously, then I moved to dozenal, while still playing around with binary every once in a while. I also briefly looked at non-geometric bases, like fibonacci bases.
+Then I ventured into irrational territory, while wandering in wikipedia. Which led me to negabinary, then, finally, to complex bases.
+And that last one is the one that stuck (aside from the ocasional binary dabbling). So much so that it became my maths-investigation in highschool.
+
+(pictures)
+
+My maths-invetigation was about 1+i as a binary base. I didn't get very far into it. I probably needed another 3 weeks to get anywhere.
+I did discover some important things though. I figured out that a base can have repeats, and it can also have numbers that don't appear at all.
+
+(pictures)
+
+After a big while, I decided to investigate it further. I found out (can't remember how) that -1+i was a more promising binary base. By that I mean it was a proper base; it can represent any complex number, with no overlap (other than the "0.999... = 1"esque overlap).
+
+(pictures)
+
+
