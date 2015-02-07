69fbffa9809f663fa7f341b352c75ba37d5cec75
42f87d89 <gbuschle@hotmail.com>
2015-02-07 18:23:56 +0000

    post2 almost done

--- Blog/index.php
+++ Blog/index.php
@@ -1,6 +1,10 @@
 <!DOCTYPE html>
 <head><link rel="stylesheet" type="text/css" href="/default.css"></head>
     <body>
-    <?php include '../navbar.php';?>
-    <p class="text">Nothing Here Yet!</p></body>
+        <?php include '../navbar.php';?>
+        <div class="main">
+            <div ><a href="./post1/">Number Systems</a></div>
+            <div ><a href="./post2/">Fractals and Terrain Generation</a></div>
+        </div>
+    </body>
 </html>
---
+++ Blog/post1/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+    <head>
+        <link rel="stylesheet" href="/default.css">
+    </head>
+    <body>
+        <?php include "../../navbar.php";?>
+        <div class="main">
+            <p>
+                Heh, I finished the second post before the first
+            </p>
+        </div>
+    </body>
+</html>
