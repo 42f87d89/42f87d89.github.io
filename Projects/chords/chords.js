9bebb6827708f926ff153e204379be7faff66e40
gustavo <gbuschle@hotmail.com>
2017-03-04 13:15:19 +0000

    decoupled chord types and buildchord func

--- Projects/chords/chords.js
+++ Projects/chords/chords.js
@@ -1,30 +1,33 @@
 var tones = ["A","A#","B","C","C#","D","D#","E","F","F#","G","G#"];
 var types = {
     "min": [0, 3, 7],
-    "maj": [0, 4, 7]
-}
+    "maj": [0, 4, 7],
+    "dim": [0, 3, 6],
+    "aug": [0, 4, 8],
+};
 
 var chord = {
     "type": "",
     "root": 0,
-}
+};
 
-function buildChord() {
-    return tones[(chord.root+types[chord.type][0])%12] + " " +
-           tones[(chord.root+types[chord.type][1])%12] + " " +
-           tones[(chord.root+types[chord.type][2])%12];
+function buildChord(chord) {
+    var result = "";
+    for(var i = 0; i<types[chord.type].length; i++) {
+        result += tones[(chord.root+types[chord.type][i])%12] + " "
+    }
+    return result;
 }
 
 function newChord() {
     document.getElementById("tones").innerText = "";
 
     chord.root = Math.floor(Math.random()*12);
-    if(Math.random() < 0.5) chord.type = "min";
-    else chord.type = "maj";
+    chord.type = Object.keys(types)[Math.floor(Math.random()*Object.keys(types).length)];
 
     document.getElementById("chord").innerText = tones[chord.root] + chord.type;
 }
 
 function showChord() {
-    document.getElementById("tones").innerText = buildChord();
+    document.getElementById("tones").innerText = buildChord(chord);
 }
